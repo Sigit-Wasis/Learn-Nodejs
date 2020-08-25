882197d3cdb928b77f33e542917b96a18e2a6c0e
Sigit-Wasis <sigitwasissubekti@MacBook-Pro-sigit.local>
2020-08-25 09:16:18 +0700

    object references

--- movie-youtube/object_references.js
+++ movie-youtube/object_references.js
@@ -1,6 +1,25 @@
+// Object awal
 var newLocal = {
     name: "sigit wasis subekti",
     umur: 23,
     keinginan: 'pejuang kode',
 };
+
+// kita rubah dengan mengcopy dari object newLocal
+var obj2 = newLocal;
+obj2.umur = 19;
+
 console.log(newLocal);
+
+/* Keterangan =
+    Jadi object references adalah kita dapat merubah object awal dengan 
+    mencopy kedalam object baru, dimna jika data kita rubah maka akan 
+    berganti sesuai data yang terdapat pada object yang baru.
+
+    Contoh:
+        Didalam object newLocal terdapat variabel umur dengan nilai 23
+        Setelah kita rubah di dalam obj2 maka nilainya akan menjadi 19
+
+    Output:
+        { name: 'sigit wasis subekti', umur: 19, keinginan: 'pejuang kode' }
+*/

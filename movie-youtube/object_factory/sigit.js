83cdbee95e9809d80a750d08b2c7dbd6e21d2920
Sigit-Wasis <sigitghoticmetal2001@gmail.com>
2020-09-09 23:35:15 +0700

    object factory nodejs

---
+++ movie-youtube/object_factory/sigit.js
@@ -0,0 +1,6 @@
+var skill = require('./skill.js');
+var skill_obj = skill();
+
+skill_obj.language = "PHP";
+
+console.log('Sigit Bisa ' + skill_obj.language);
---
+++ movie-youtube/object_factory/wasis.js
@@ -0,0 +1,6 @@
+var skill = require('./skill.js');
+var skill_obj = skill();
+
+skill_obj.language = "Node js"
+
+console.log('Wasis Bisa ' + skill_obj.language);
